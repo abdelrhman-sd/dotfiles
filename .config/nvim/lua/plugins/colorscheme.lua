6f9c1b77004a57c965d286a0fc55daf9fe731506
abdelrhman-sd <abdelrhmansaeed001@gmail.com>
2026-06-15 07:23:04 +0300

    fix: fixing the "slugbyte/lackluster.nvim" active table background color issue

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -45,26 +45,69 @@ return {
     "slugbyte/lackluster.nvim",
     lazy = false,
     priority = 1000,
-    --config = function()
-    --  local lackluster = require("lackluster")
-    --  lackluster.setup({
-    --    tweak_color = {
-    --      lack = "default",
-    --      luster = "default",
-    --      orange = "default",
-    --      yellow = "default",
-    --      green = "default",
-    --      blue = "default",
-    --      red = "default",
-    --    },
-    --    tweak_background = {
-    --      normal = "none",
-    --      telescope = "none",
-    --      menu = "none",
-    --      popup = "none",
-    --    },
-    --  })
-    --end,
+    config = function()
+      local lackluster = require("lackluster")
+      local color = lackluster.color
+
+      require("nvim-web-devicons").setup({
+        color_icons = false,
+        override = {
+          ["default_icon"] = {
+            color = "none",
+            name = "Default",
+          },
+        },
+      })
+
+      lackluster.setup({
+        disable_plugin = {
+          bufferline = true,
+          cmp = false,
+          dashboard = false,
+          flash = false,
+          git_gutter = false,
+          git_signs = false,
+          headline = false,
+          indentmini = false,
+          lazy = false,
+          lightbulb = false,
+          lsp_config = false,
+          mason = false,
+          mini_diff = false,
+          navic = false,
+          noice = false,
+          notify = false,
+          oil = false,
+          rainbow_delimiter = false,
+          scollbar = false,
+          telescope = false,
+          todo_comments = false,
+          tree = false,
+          trouble = false,
+          which_key = false,
+          yanky = false,
+        },
+        tweak_syntax = {
+          string = "default",
+          string_escape = "default",
+          comment = "default",
+          builtin = "default",
+          type = "default",
+          keyword = "default",
+          keyword_return = "default",
+          keyword_exception = "default",
+        },
+        tweak_background = {
+          telescope = "default",
+          menu = "default",
+          popup = "default",
+        },
+      })
+
+      -- Remove the vertical separator line and indicator line backgrounds
+      vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", { bg = color.gray3, fg = color.gray3 })
+      vim.api.nvim_set_hl(0, "BufferLineIndicatorSelected", { bg = color.gray3, fg = color.gray3 })
+    end,
   },
 
   {
